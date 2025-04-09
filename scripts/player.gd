1182bc515f04ea7c995f4a4786b86fbff6abe58c
Paulinux <paul1retif@gmail.com>
2025-04-09 19:22:20 +0200

    Fix move speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 class_name Player
 
-var move_speed : float = 5000
+var move_speed : float = 700
 var gravity : float = 3000
-var jump_force : float = 1000
+var jump_force : float = 5000
 
 @onready var animated_sprite = $AnimatedSprite2D
 #@onready var stateMachine = animated_sprite.get("parameters/playback")
@@ -12,7 +12,7 @@ var active = true
 func _physics_process(delta: float) -> void:
 	
 	if !is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity
 		if velocity.y > 500:
 			velocity.y = 500
 	var input_direction
@@ -20,7 +20,7 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_just_pressed("jump") && is_on_floor():
 			jump(jump_force)
 		input_direction = Input.get_axis("move_left", "move_right")
-		velocity.x = input_direction * move_speed * delta
+		velocity.x = input_direction * move_speed
 	
 	if input_direction != 0:
 		animated_sprite.flip_h = (input_direction == -1)
